9efa4ab8f13a8444ac80bf4aa9038d8eb20b2519
Vinicius Albano <vinialbano@gmail.com>
2021-07-28 13:04:01 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,12 +3,13 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
+  // @ts-expect-error HttpResponse return is undefined
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
-    // if (!httpRequest.body.email) {
-    return badRequest(new MissingParamError('email'))
-    // }
   }
 }
