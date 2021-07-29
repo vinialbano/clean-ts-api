0d64b76409195271bdd22c75d0dfefddc8a0bb20
Vinicius Albano <vinialbano@gmail.com>
2021-07-28 22:47:06 -0300

    test: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -90,13 +90,14 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
-        password: 'any_password'
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(
-      new MissingParamError('passwordConfirmation')
+      new InvalidParamError('passwordConfirmation')
     )
   })
 
